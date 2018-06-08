7ead08dd7cc4507fd558039e0df2f945a063d6f6
Marie-Clare Trotta <marieclare.t@hotmail.ca>
2018-06-08 15:50:48 -0400

    made a get function page

---
+++ wp-content/themes/child-of-twentysixteen/function.php
@@ -0,0 +1,16 @@
+<?php 
+
+function my_theme_enqueue_styles() {
+	$parent_style = 'twentysixteen-style'; //This is 'twentysixteen-style for the twenty sixteen theme.'
+
+	wp_enqueue_style( $parent_style,
+		get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		arry( $parent_style ),
+		wp_get_theme()->get('Version')
+	);
+}
+
+add_action( 'wp_enqueue_scripts' , '
+	my_theme_enqueue_stylles' );
